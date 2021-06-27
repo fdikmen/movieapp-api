51ac996fc5a6563b2952e2851ffadab98d0f6080
frtdkmn <frtdkmn@gmail.com>
2021-06-27 15:34:37 +0300

    Added user schema and routws

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+//Models
+const UserModel=require('../models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  UserModel.find((err, data) => {
+    if (err) res.json(err);
+    res.json(data);
+  })
+});
+
+
+
+router.post('/register', (req, res) => {
+  const {username,password} = req.body;
+const newUser = new UserModel({username,password});
+newUser.save()
+         .then((data)=>{res.json(data)})
+         .catch((err)=>{res.json(err)})
+
 });
 
 module.exports = router;
